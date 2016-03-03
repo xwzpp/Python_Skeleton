3ad5817e77c73dbc2a2f11d552afbe63ca7861bb
xuwei <xwnjnu@163.com>
2016-03-03 14:24:43 +0800

    init Python_Skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding=utf-8
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+	
+
+config={
+	'description': 'Python_Skeleton',
+	'author': 'XuWei',
+	'url': 'Url to get it',
+	'download_url': 'Where to download it',
+	'author_email': 'xwnjnu@gmail.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'Python_Skeleton'
+}
+
+setup(**config)
